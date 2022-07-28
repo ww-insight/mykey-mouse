672c3962180c2de0022c0d66436447db3a35e2d3
wwbel <ww.bel@ya.ru>
2022-07-28 12:52:41 +0300

    moving area limited

--- main.py
+++ main.py
@@ -4,8 +4,15 @@ import random
 import logging
 from smoose import get_curve
 
+# time settings
 inactiveMin = 2
 inactiveMaxShiftSec = 30
+
+# mouse move area settings
+minX = minY = 50
+maxX = maxY = 1000
+random_shift = 100
+
 logging.basicConfig(level=logging.INFO, format="%(asctime)s\t%(levelname)s\t%(message)s")
 
 mouse = Controller()
@@ -25,8 +32,14 @@ while True:
         points = list()
         new_points = mouse.position
         for i in range(random.randint(50, 100)):
-            new_points = (new_points[0] + random.randint(-100, 100),
-                          new_points[1] + random.randint(-100, 100)
+            new_points = (new_points[0] + random.uniform(-random_shift, random_shift),
+                          new_points[1] + random.uniform(-random_shift, random_shift)
+                          )
+            new_points = ((new_points[0] if new_points[0] > minX else random.uniform(minX, minX + random_shift)),
+                          (new_points[1] if new_points[1] > minY else random.uniform(minY, minY + random_shift))
+                          )
+            new_points = ((new_points[0] if new_points[0] < maxX else random.uniform(maxX - random_shift, maxX)),
+                          (new_points[1] if new_points[1] < maxY else random.uniform(maxY - random_shift, maxY))
                           )
             points.append(new_points)
 
